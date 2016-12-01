b70227fe00c28b368c7e709799ac6728a20eb005
EsGeh <SamuelGfroerer@googlemail.com>
2016-12-02 00:34:56 +0100

    updated stuff for exercise6. TODO: update doc

--- doc/createPDF/generate.sh
+++ doc/createPDF/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-OUTPUT=Uebung5.pdf
-INPUT=Uebung5.md
+INPUT="$(echo *.md)"
+OUTPUT="$(basename $INPUT .md)".pdf
 
 pandoc --number-offset=5 --number-sections -o "$OUTPUT" "createPDF/header" "$INPUT"
 # pandoc --filter pandoc-citeproc --include-in-header="./createPDF/header" --number-sections -o "$OUTPUT" $(echo "$INPUT")
